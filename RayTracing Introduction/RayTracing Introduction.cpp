e112a06aaa4524a38874d57e625f81a00e94ea9d
ZoeChrvx <zchervaux@artfx.fr>
2024-01-18 15:27:00 +0100

    [add] Dialectrics (glass mat)

--- RayTracing Introduction/RayTracing Introduction.cpp
+++ RayTracing Introduction/RayTracing Introduction.cpp
@@ -2,6 +2,8 @@
 #include "HittableCollection.h"
 #include "Sphere.h"
 #include "Camera.h"
+
+#include "DialectricMaterial.h"
 #include "LambertianMaterial.h"
 #include "MetalMaterial.h"
 
@@ -13,9 +15,9 @@ int main(int argc, char* argv[])
     HittableCollection world;
 
     shared_ptr<Material> groundMat = make_shared<LambertianMaterial>(Color(0.8, 0.8, 0.0));
-    shared_ptr<Material> centerMat = make_shared<LambertianMaterial>(Color(0.7, 0.3, 0.3));
-    shared_ptr<Material> leftMat = make_shared<MetalMaterial>(Color(0.8, 0.8, 0.8), 1.0);
-    shared_ptr<Material> rightMat = make_shared<MetalMaterial>(Color(0.8, 0.6, 0.2), 0.3);
+    shared_ptr<Material> leftMat = make_shared<DialectricMaterial>(1.5);
+    shared_ptr<Material> centerMat = make_shared<LambertianMaterial>(Color(0.1, 0.2, 0.5));
+    shared_ptr<Material> rightMat = make_shared<MetalMaterial>(Color(0.8, 0.6, 0.2), 0.0);
 
     world.Add(make_shared<Sphere>(Position(0, -100.5, -1), 100, groundMat));
     world.Add(make_shared<Sphere>(Position(0, 0, -1), 0.5, centerMat));
